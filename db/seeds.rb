4abaeaeaf51a553922ca88af4020a892b406cdb6
CatonNip <vivinguyen000@gmail.com>
2016-11-04 11:25:19 -0700

    make it better

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-users = 20.times.map do
-  User.create!( :username  => Faker::Internet.user_name,
-                :email      => Faker::Internet.email,
-                :password   => 'password' )
-end
